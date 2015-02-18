2a1028312b9c7e1d9fc19b917193d2e8e546dafe
Soulou <leo@unbekandt.eu>
2015-02-18 14:47:30 +0100

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# HTTP Hijack client exemple
+
+## Usage
+
+`go-http-hijack-client <URL>`
+
+It sends stdin to server and display servers output to stdout
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("usage:", os.Args[0], "<url>")
+	}
+	rawURL := os.Args[1]
+
+	url, err := url.Parse(rawURL)
+	if err != nil {
+		log.Fatalln(rawURL, "is not a valid URL", err)
+	}
+
+	req, err := http.NewRequest("CONNECT", rawURL, nil)
+	if err != nil {
+		log.Fatalln("Fail to create request CONNECT", rawURL)
+	}
+
+	dial, err := net.Dial("tcp", url.Host)
+	if err != nil {
+		log.Fatalln("Fail to connect to", url.Host, err)
+	}
+
+	var conn *httputil.ClientConn
+	if url.Scheme == "https" {
+		host := strings.Split(url.Host, ":")[0]
+		tls_conn := tls.Client(dial, &tls.Config{ServerName: host})
+		conn = httputil.NewClientConn(tls_conn, nil)
+	} else if url.Scheme == "http" {
+		conn = httputil.NewClientConn(dial, nil)
+	} else {
+		log.Println("Scheme format should be 'http' or 'https' not", url.Scheme)
+	}
+
+	_, err = conn.Do(req)
+	if err != httputil.ErrPersistEOF && err != nil {
+		log.Fatalln("Fail to execute http request", err)
+	}
+
+	log.Println("Hijack connection")
+	connection, _ := conn.Hijack()
+
+	fmt.Println("Pipe stdin to socket, and socket to stdout")
+	go io.Copy(connection, os.Stdin)
+	_, err = io.Copy(&ServerWriter{os.Stdout}, connection)
+	if err != nil {
+		log.Println("Erreur when copying socket to stdout:", err)
+	}
+	fmt.Println("End of hijacking")
+}
+
+type ServerWriter struct{ io.Writer }
+
+func (w *ServerWriter) Write(b []byte) (int, error) {
+	w.Writer.Write([]byte("Server: "))
+	return w.Writer.Write(b)
+}
